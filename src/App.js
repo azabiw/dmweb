b1afffefc57807a60f192f73be022560dd61d222
ossi <ozzivanhala@gmail.com>
2019-12-11 20:50:22 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import {TextField} from "@material-ui/core";
+
+
+const Header = (props) => {
+  return (
+      <nav>
+          Linkit halutuille sivuille.
+      </nav>
+  )
+}
+
+
+function tallenna() {
+    console.log("Tallenna");
+}
+
+
+const Field = (props) => {
+    return (
+        <div className="InputFieldContainer">
+            <label className="FieldLabel">{props.label}</label>
+            <TextField>{props.text}</TextField>
+        </div>
+    )
+}
+
+
+const Footer = (props) => {
+  return (
+      <footer>
+        Footer
+      </footer>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
+        <form>
+            <Field label={"Name"} text={""}/>
+            <Field label={"Race"} text={""}/>
+            <Field label={"Class"} text={""}/>
+            <Field label={"Age"} text={""}/>
+            <Button className="saveButton" variant="contained" color="primary" onClick={tallenna}>
+                Save
+            </Button>
+        </form>
+        <Footer/>
     </div>
   );
 }
