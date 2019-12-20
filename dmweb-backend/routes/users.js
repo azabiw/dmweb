f9b848ad39f0b3b9b309637a60756aa48fb13ff6
Ossi Vanhala <ozzivanhala@gmail.com>
2019-12-20 23:12:22 +0200

    datan hakeminen template

--- dmweb-backend/routes/users.js
+++ dmweb-backend/routes/users.js
@@ -4,18 +4,9 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   //res.send('respond with a resource');
-  connectToDB();
-  res.json([{
-    id: 1,
-    username: "Character 1"
-  }, {
-    id: 2,
-    username: "Character 2"
-  }]);
-});
-function connectToDB() {
+  let result = connectToDB();
+
   let MongoClient = require('mongodb').MongoClient;
-  insertToDB();
   MongoClient.connect('mongodb://localhost:27017/dmweb', function (err, client) {
     if (err) throw err;
 
@@ -23,8 +14,24 @@ function connectToDB() {
 
     db.collection('dmweb').find().toArray(function (err, result) {
       if (err) throw err;
-
       console.log(result);
+      res.send(result);
+    })
+  });
+
+
+  //res.send(connectToDB());
+});
+function connectToDB() {
+  let MongoClient = require('mongodb').MongoClient;
+  MongoClient.connect('mongodb://localhost:27017/dmweb', function (err, client) {
+    if (err) throw err;
+
+    let db = client.db('dmweb');
+
+    db.collection('dmweb').find().toArray(function (err, result) {
+      if (err) throw err;
+      return result;
     })
   })
 }
