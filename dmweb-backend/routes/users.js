712cb22829f036a1fae3fc754bc7d54e0cccfe44
Ossi Vanhala <ozzivanhala@gmail.com>
2019-12-20 11:41:34 +0200

    Mongo connect toimii

--- dmweb-backend/routes/users.js
+++ dmweb-backend/routes/users.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   //res.send('respond with a resource');
+  connectToDB();
   res.json([{
     id: 1,
     username: "Character 1"
@@ -12,5 +13,19 @@ router.get('/', function(req, res, next) {
     username: "Character 2"
   }]);
 });
+function connectToDB() {
+  let MongoClient = require('mongodb').MongoClient;
 
+  MongoClient.connect('mongodb://localhost:27017/dmweb', function (err, client) {
+    if (err) throw err;
+
+    let db = client.db('dmweb');
+
+    db.collection('dmweb').find().toArray(function (err, result) {
+      if (err) throw err;
+
+      console.log(result);
+    })
+  })
+}
 module.exports = router;
