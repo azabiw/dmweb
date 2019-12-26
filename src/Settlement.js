d85b820ab75fc15c55dfff2a1df5c5ef82ca5f96
Ossi Vanhala <ozzivanhala@gmail.com>
2019-12-26 21:41:36 +0200

    Kaupunkilomake

--- src/Settlement.js
+++ src/Settlement.js
@@ -10,17 +10,30 @@ class Settlement extends React.Component {
     characters = [];
     render() {
         return (
-            <form id="inputForm">
-                <Field label={"Name"} text={""}/>
-                <select className="SettlementLeaderSelector">
-                    <option value="testi">Character 1</option>
-                    <option value="testi2">Character 2</option>
-                    <option value="testi">Character 3</option>
-                </select>
-                <Button className="saveButton" variant="contained" color="primary" onClick={utilities.save}>
-                    Save
-                </Button>
-            </form>
+            <fieldset >
+                <form id="inputForm">
+                    <Field label={"Name"} text={""}/>
+                    <select className="SettlementLeaderSelector">
+                        <option value="testi">Character 1</option>
+                        <option value="testi2">Character 2</option>
+                        <option value="testi">Character 3</option>
+                    </select>
+                    <Field label={"Location"} text={""}/>
+                    <Field label={"Noticeable Features"} text={""}/>
+                    <Field label={"Population and Structure"} text={""}/>
+                    <Field label={"Security"} text={""}/>
+                    <Field label={"Organisations"} text={""}/>
+                    <Field label={"Interesting locations"} text={""}/>
+                    <Field label={"Plot Hooks"} text={""}/>
+
+
+
+
+                    <Button className="saveButton" variant="contained" color="primary" onClick={utilities.save}>
+                        Save
+                    </Button>
+                </form>
+            </fieldset>
         )
     }
 
