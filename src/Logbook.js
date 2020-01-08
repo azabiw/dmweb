abd80921ad896b8c8fda02ad97878b0d15b40660
Ossi Vanhala <ozzivanhala@gmail.com>
2020-01-08 16:48:50 +0200

    lisätty props ja routing

--- src/Logbook.js
+++ src/Logbook.js
@@ -3,7 +3,7 @@ import React from "react";
 
 class Logbook extends React.Component {
     constructor(props) {
-        super();
+        super(props);
         this.addEntry = this.addEntry.bind(this);
         this.removeEntry = this.removeEntry.bind(this);
         this.state = {
