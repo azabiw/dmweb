37843e569c93164379c956b4be1eb83897fe83ff
Ossi Vanhala <ozzivanhala@gmail.com>
2020-01-08 21:32:20 +0200

    Header placeholder tyyli

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,30 @@ import styles from "../styles/Header.module.css";
 class Header extends React.Component {
 
     render() {
+        //pakko määritellä näin, koska Link ei jostain syystä ymmärrä CSS -moduuleja
+        const style = {
+            "text-decoration": "none",
+        "font-size": "1.5rem",
+        display: "inline-block",
+        margin: "1rem 0.5rem 1rem 0.5rem",
+        padding: "1rem",
+        color: "white",
+        "border-radius" : "1rem",
+        "background-color": "darkgray"
+    };
         return (
             <nav className={styles.header}>
                 <ul>
-                    <li className={styles.Link}>
-                        <Link to="/">Front page</Link>
+                    <li className={styles.ListElement}>
+                        <Link style={style} to="/">Front page</Link>
                     </li>
-                    <li className={styles.Link}>
-                        <Link to="/editor">Editor</Link>
+                    <li className={styles.ListElement}>
+                        <Link style={style} to="/editor">Editor</Link>
                     </li>
-                    <li className={styles.Link}>
-                        <Link to="/about">About</Link>
+                    <li className={styles.ListElement}>
+                        <Link style={style} classname={styles.LinkContainer} to="/about">About</Link>
                     </li>
+
                 </ul>
             </nav>
         )
