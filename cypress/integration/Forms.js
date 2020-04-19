7cf0aa4408535c1a7eb97a5c875febadae279e85
Ossi Vanhala <ozzivanhala@gmail.com>
2020-04-19 19:12:16 +0300

    testataan hahmon toiminnot

--- cypress/integration/Forms.js
+++ cypress/integration/Forms.js
@@ -25,4 +25,37 @@ describe("Open character editor", () => {
         cy.visit('127.0.0.1:3000');
         cy.contains("NPC Editor").click();
     })
+    it("New character button works", () => {
+        cy.contains("Add a new Character").click({force:true});
+    })
+    it("Character can be added", () => {
+        cy.get('#name').type("testihahmo");
+        cy.contains("Save").click();
+        cy.contains("Settlement Editor").click();
+        cy.contains("testihahmo");
+    })
+    it("Character can be removed", () => {
+        cy.contains("testihahmo").click();
+        cy.contains("Remove").click();
+        cy.contains('testihahmo').should('not.exist');
+    })
+    it("Character can be added", () => {
+        cy.visit('127.0.0.1:3000');
+        cy.contains("NPC Editor").click();
+        cy.contains("Add a new Character").click({force:true});
+        cy.get('#name').type("testihahmo");
+        cy.contains("Save").click();
+        cy.contains("Settlement Editor").click();
+        cy.contains("testihahmo");
+        cy.contains("testihahmo").click();
+        cy.get(':nth-child(2) > input').type("Lisäys");
+        cy.contains("Save").click();
+        cy.contains("Settlement Editor").click();
+        cy.contains("testihahmo");
+        cy.contains("testihahmo").click();
+        cy.contains("Lisäys");
+        cy.contains("Remove").click();
+        cy.contains('testihahmo').should('not.exist');
+
+    })
 })
