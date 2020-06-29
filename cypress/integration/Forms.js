6c81607842deb5e1da21694521851e45859acf4b
Ossi Vanhala <ozzivanhala@gmail.com>
2020-06-29 16:24:31 +0300

    lisätty cypressiin firebase-plugin

--- cypress/integration/Forms.js
+++ cypress/integration/Forms.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /// <reference types="Cypress" />
 describe('Different pages work', () => {
     it('frontpage opens', () => {
@@ -49,3 +50,9 @@ describe("Hp counter works", () => {
         cy.get(':nth-child(1) > :nth-child(2) > .segment').contains("113");
     })
 })
+
+describe("Some Test", () => {
+    it("Adds document to test_hello_world collection of Firestore", () => {
+      cy.callFirestore("add", "test_hello_world", { some: "value" });
+    });
+  });
