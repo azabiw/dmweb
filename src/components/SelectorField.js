2a9a3ee7a1a68f695d469c078c03b77fa013a03c
Ossi Vanhala <ozzivanhala@gmail.com>
2020-07-08 18:02:24 +0300

    workaround tilalle

--- src/components/SelectorField.js
+++ src/components/SelectorField.js
@@ -11,19 +11,40 @@ class SelectorField extends React.Component {
     constructor(props) {
         super(props);
         let idOfDefault = ( this.props.idOfDefault != null) ? this.props.idOfDefault : "Not selected";
-        let properties = this.props.properties;
+        let properties = this.props.properties ?? [];
         console.log("proops", properties);
         this.state = {
             properties: properties,
             idOfDefault: idOfDefault
         }
         //properties for example characters or settlements
+
+        this.componentWillReceiveProps = this.componentWillReceiveProps.bind(this);
+    }
+
+    /** TODO: KORJAA
+     * workaround tilan muuttamiseksi. 
+     * Tämä ei ole tehon kannalta suositeltava ratkaisu, mutta ottaen huomioon lomakkeiden määrän, vaikutus ei ole kovinkaan vakava.
+     * Voidaan ratkaista hakemalla redux storesta arvot, mutta se lisää muistin käyttöä vastaavassa määrin.
+     * @param {} nextProps 
+     */
+    componentWillReceiveProps(nextProps){
+        this.setState({
+            properties: nextProps.properties
+        })
     }
 
     render() {
-        const propertyList = this.props.properties.map((property) =>
+        let propertyList;
+        if (this.state.properties !== undefined) {
+            propertyList = this.state.properties.map((property) =>
             <option value={property.id}> {property.name ? property.name : "No name"} </option>
-        );
+            );
+        } else {
+            return <div>
+
+            </div>
+        }
         return (
             <div>
                 <label>{this.props.label}</label>
